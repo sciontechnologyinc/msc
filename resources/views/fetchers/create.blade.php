8fd082763e1cd8f04463a609a1d6f6476489d0c6
Jetro Macalipay <wajjfreelance@gmail.com>
2018-10-19 03:10:46 +0800

    test
    test

--- resources/views/fetchers/create.blade.php
+++ resources/views/fetchers/create.blade.php
@@ -4,7 +4,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Add Teacher</h4>
+          <h4 class="modal-title">Add Fetcher</h4>
         </div>
         <div class="modal-body">
             {!! Form::open(['id' => 'dataForm', 'url' => 'fetcher/save']) !!}
